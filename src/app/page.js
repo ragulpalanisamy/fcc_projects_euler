bbf11777efe31b1e5ccf61abe4325f8d10cb5e2b
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-12-03 22:01:24 +0530

    feat: :sparkles: added the largest prime factor

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
+'use client'
 import Link from 'next/link'
 
 export default function Home() {
-  return  <Link href="/Even_Fibonacci_Numbers">
-        <a>Even_Fibonacci_Numbers</a>
+  return  (
+  <>
+   
+  <Link href="/Even_Fibonacci_Numbers" style={{ marginRight: 10  }}>
+      Even_Fibonacci_Numbers
+    </Link>
+    <br></br>
+    <Link href="/Multiples">
+        Multiples
       </Link>
+    <br></br>
+    <Link href="/Largest">
+        Largest
+      </Link>
+      </>
+
+  )
 }
