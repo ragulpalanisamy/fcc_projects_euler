cbb81096235b7c651e57ec880ec20ee0673d2455
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-12-05 22:44:31 +0530

    feat: :sparkles: Added the pythogorean triplet

---
+++ src/pages/Pythagorean_triplet.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+
+export default function Pythagorean_triplet() {
+    const codesnippet = `
+    function specialPythagoreanTriplet(n) {
+        let sumOfabc = n;
+        for (let a = 1; a < n; a++) {
+          for (let b = a; b < n; b++) {
+            let c = n - a - b;
+            if (c > 0) {
+              if (c ** 2 == a ** 2 + b ** 2) {
+                return a * b * c;
+              }
+            }
+          }
+        }
+      }
+      
+      specialPythagoreanTriplet(1000);
+    `;
+    function specialPythagoreanTriplet(n) {
+        let sumOfabc = n;
+        for (let a = 1; a < n; a++) {
+            for (let b = a; b < n; b++) {
+                let c = n - a - b;
+                if (c > 0) {
+                    if (c ** 2 == a ** 2 + b ** 2) {
+                        return a * b * c;
+                    }
+                }
+            }
+        }
+    }
+
+    specialPythagoreanTriplet(1000);
+    return (
+        <div>
+            <h1>
+                Pythagorean_triplet
+            </h1>
+            <pre>
+                <code>
+                    {codesnippet}
+                </code>
+            </pre>
+            <p>
+                {specialPythagoreanTriplet(1000)}
+            </p>
+        </div>
+    )
+}
