f9a9a393c212d4e0607f99124e2a52c5f20f4bf6
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-12-13 21:27:47 +0530

    feat: :sparkles: Added the lexicographic permutation

---
+++ src/pages/Pro-2/Lexicographic_permutations.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function Lexicographic_permutations() {
+    const codesnippet = `
+    const FACTORIALS = Array(9)
+  .fill(0)
+  .reduce((factorials, _, indx) => {
+    factorials.push(factorials[indx] * (indx + 1));
+    return factorials;
+  },
+  [1])
+  .reverse();
+
+function lexicographicPermutations(n) {
+  const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let permutation = "";
+  for (let i = 0; i < 10; i++) {
+    permutation += digits.splice(Math.floor(n / FACTORIALS[i]), 1);
+    n = n % FACTORIALS[i];
+  }
+  return Number(permutation);
+}
+    `;
+
+    const FACTORIALS = Array(9)
+        .fill(0)
+        .reduce((factorials, _, indx) => {
+            factorials.push(factorials[indx] * (indx + 1));
+            return factorials;
+        },
+            [1])
+        .reverse();
+
+    function lexicographicPermutations(n) {
+        const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let permutation = "";
+        for (let i = 0; i < 10; i++) {
+            permutation += digits.splice(Math.floor(n / FACTORIALS[i]), 1);
+            n = n % FACTORIALS[i];
+        }
+        return Number(permutation);
+    }
+    return (
+        <div>
+            <h1>
+                Lexicographic_permutations
+            </h1>
+            <pre>
+                <code>
+                    {codesnippet}
+                </code>
+            </pre>
+            <p>
+                {lexicographicPermutations(362880)}
+            </p>
+        </div>
+    )
+}
